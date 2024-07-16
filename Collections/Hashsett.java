35b654053437f178249406a153f11d909f5f2ebf
sushil-18 <sushilkumarwaghmare9@gmail.com>
2024-07-16 23:32:15 +0530

    Implemented hashset

---
+++ Collections/Hashsett.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Hashsett {
+
+    public static void main(String[] args) {
+        HashSet<String> h = new HashSet<String>();
+
+        h.add("India");
+        h.add("Australia");
+        h.add("South Africa");
+
+        h.add("India");
+
+        System.out.println(h);
+        System.out.println("List contains India or not:" + h.contains("India"));
+
+        h.remove("Australia");
+        System.out.println("List after removing Australia:" + h);
+
+        System.out.println("Iterating over list:");
+
+        // Iterating over hashSet items
+        Iterator<String> i = h.iterator();
+
+        while (i.hasNext()) System.out.println(i.next());
+    }
+}
